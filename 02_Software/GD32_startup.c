5ea0408c470b681184bb927b67f1d97ca095f624
Lukas Fuereder <l.fuereder@web.de>
2025-11-17 22:40:46 +0100

    added apache license notice to the source files

--- 02_Software/GD32_startup.c
+++ 02_Software/GD32_startup.c
@@ -1,3 +1,29 @@
+/*!
+    \file    GD32_startup.c
+    \brief   startup code of the IN14-nixie-clock module 
+
+    \version 2025-11-17, V1.0.0, IN14_NIXIE_CLOCK_MODULE
+*/
+
+/*
+
+/*
+   Copyright 2025 Lukas Füreder
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
 #ifndef int16_t
     typedef short int                   int16_t;
 #endif
